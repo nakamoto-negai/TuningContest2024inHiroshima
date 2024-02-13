5726ab6ed858af5fad8ec0fb144e6c5c1e8e279e
noda1111 <159742007+noda1111@users.noreply.github.com>
2024-02-13 13:59:23 +0900

    0_sample.sql

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,1 +1,7 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+CREATE INDEX idx_mail_pass ON user (mail,password);
+CREATE INDEX idx_role ON department_role_member (role_id,belong);
+CREATE INDEX idx_department_role_member_department_id_user_id_belong 
+ON department_role_member (department_id, user_id, belong);
+CREATE INDEX idx_user_id ON match_group_member (user_id);
+CREATE INDEX idx_entry_date_kana ON user (entry_date, kana);
